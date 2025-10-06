50bfa71b8ac20e09dab47831181820464466ba5d
iinemo <artemshelmanov@gmail.com>
2025-10-06 04:45:39 +0400

    Change API

--- llm_tts/step_candidate_generator_through_api.py
+++ llm_tts/step_candidate_generator_through_api.py
@@ -12,6 +12,7 @@ from lm_polygraph import BlackboxModel
 from llm_tts.step_candidate_generator_base import (
     StepCandidate,
     StepCandidateGeneratorBase,
+    covert_trajectory_to_string
 )
 from llm_tts.step_boundary_detector import StepBoundaryDetector
 
@@ -32,25 +33,19 @@ class StepCandidateGeneratorThroughAPI(StepCandidateGeneratorBase):
         self.prefill_mode = prefill_mode
 
     def generate_candidates(
-        self, request: List[Dict[str, str]], trajectory: str, candidates_per_step: int
+        self,
+        request: List[Dict[str, str]],
+        trajectory: List[StepCandidate],
+        candidates_per_step: int,
     ) -> List[StepCandidate]:
         """Generate N candidate next steps from current trajectory"""
 
         log.info(f"Generating {candidates_per_step} candidates from trajectory")
 
-        candidates = []
-
         start_time = time.time()
         
-        request_with_trajectory = copy.deepcopy(request)
-
-        if not trajectory:
-            request_with_trajectory = request
-        else:
-            if not self.prefill_mode:
-                request_with_trajectory = self._add_prefix_to_request(request_with_trajectory, trajectory)
-            else:
-                request_with_trajectory.append({"role": "assistant", "content": trajectory, "prefix": True})
+        candidates = []
+        request_with_trajectory = self._prepare_request(request, trajectory)
 
         # Generate multiple candidates by making multiple API calls
         for i in range(candidates_per_step):
@@ -94,11 +89,26 @@ class StepCandidateGeneratorThroughAPI(StepCandidateGeneratorBase):
         return candidates
 
     def generate_answer_candidates(
-        self, request: List[Dict[str, str]], trajectory: str, candidates_per_step: int
+        self,
+        request: List[Dict[str, str]],
+        trajectory: List[StepCandidate],
+        candidates_per_step: int,
     ) -> List[StepCandidate]:
         """Generate and select best final answer based on criterion"""
-        
-        candidates = self.generate_candidates(request, trajectory.rstrip() + "\n<Answer>:\n", candidates_per_step)
+
+        final_trajectory = [e for e in trajectory]
+        final_trajectory.append(StepCandidate(
+            text="\n<Answer>:\n", # TODO: get configuration from the step boundary detector
+            token_ids=[],
+            is_complete=False,
+            is_trajectory_complete=False,
+            generation_scores=None,
+            raw_text="\n<Answer>:\n",
+        ))
+
+        candidates = self.generate_candidates(
+            request, final_trajectory, candidates_per_step
+        )
         for cand in candidates:
             cand.is_trajectory_complete = True
             cand.text = "\n<Answer>:\n" + cand.text
@@ -106,20 +116,46 @@ class StepCandidateGeneratorThroughAPI(StepCandidateGeneratorBase):
 
         return candidates
 
-    def _add_prefix_to_request(self, request: List[Dict[str, str]], trajectory: str):
-        continuation_request = copy.deepcopy(request)
-        #prefix = "\nAssistant: " + trajectory 
-        prefix = trajectory
-        continuation_promt = "Continue the assistant message from the EXACT prefix below. "\
-                             "Begin immediately after the last character of the prefix. "\
-                             "Output ONLY the final answer text; do NOT include steps, chain-of-thought, or any preface. "\
-                             "Do NOT repeat the prefix.\n"\
-                             "----- PREFIX START -----\n"\
-                             f"{prefix}\n"\
-                             "----- PREFIX END -----"
+    def _prepare_request(
+        self, request: List[Dict[str, str]], trajectory: List[StepCandidate]
+    ):
+        request_with_trajectory = copy.deepcopy(request)
 
-        log.debug(f"Continuation prompt: {continuation_promt}")                 
+        if not trajectory:
+            request_with_trajectory = request
+
+        else:
+            if not self.prefill_mode:
+                request_with_trajectory = self._add_prefix_to_request(
+                    request_with_trajectory, trajectory
+                )
+            else:
+                request_with_trajectory.append(
+                    {
+                        "role": "assistant",
+                        "content": covert_trajectory_to_string(trajectory),
+                        "prefix": True,
+                    }
+                )
+
+        return request_with_trajectory
+
+    def _add_prefix_to_request(
+        self, request: List[Dict[str, str]], trajectory: List[StepCandidate]
+    ):
+        continuation_request = request
+        prefix = covert_trajectory_to_string(trajectory)
+        continuation_promt = (
+            "Continue the assistant message from the EXACT prefix below. "
+            "Begin immediately after the last character of the prefix. "
+            "Output ONLY the final answer text; do NOT include steps, chain-of-thought, or any preface. "
+            "Do NOT repeat the prefix.\n"
+            "----- PREFIX START -----\n"
+            f"{prefix}\n"
+            "----- PREFIX END -----"
+        )
+
+        log.debug(f"Continuation prompt: {continuation_promt}")
         continuation_request.append({"role": "user", "content": continuation_promt})
-        
-        return continuation_request
 
+        return continuation_request
