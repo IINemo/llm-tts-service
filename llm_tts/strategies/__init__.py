21f0b80988eedaab1c2554d650ec987afbdedcbf
ssenichev <s.senichev@mts.ai>
2025-10-13 23:26:24 +0300

    feat: add new strategy

--- llm_tts/strategies/__init__.py
+++ llm_tts/strategies/__init__.py
@@ -3,6 +3,7 @@ from .strategy_chain_of_thought import StrategyChainOfThought
 from .strategy_deepconf import StrategyDeepConf
 from .strategy_online_best_of_n import StrategyOnlineBestOfN
 from .strategy_self_consistency import StrategySelfConsistency
+from .strategy_uncertainty_cot import UncertaintyGuidedCoT_PD
 
 __all__ = [
     "StrategyOnlineBestOfN",
@@ -10,4 +11,5 @@ __all__ = [
     "StrategySelfConsistency",
     "StrategyDeepThinkConfidence",
     "StrategyDeepConf",
+    "UncertaintyGuidedCoT_PD",
 ]
