2cf10a2b5bb3dd84887b7a556136e0e384c559f2
nqchieutb01 <quangchieu180901@gmail.com>
2025-10-15 18:11:04 +0400

    add 2 test dataset: MATH, ProofNet

--- scripts/run_tts_eval.py
+++ scripts/run_tts_eval.py
@@ -8,7 +8,7 @@ from pathlib import Path
 import hydra
 import numpy as np
 import torch
-from datasets import Dataset, load_dataset
+from datasets import Dataset, load_dataset, get_dataset_config_names,concatenate_datasets
 from hydra.core.hydra_config import HydraConfig
 from lm_polygraph import WhiteboxModel
 from lm_polygraph.utils.generation_parameters import GenerationParameters
@@ -442,12 +442,37 @@ def main(config):
     log.info(
         f"Loading dataset: {config.dataset.dataset_path} ({config.dataset.dataset_split})"
     )
-    dataset = load_dataset(
-        config.dataset.dataset_path,
-        config.dataset.dataset_config,
-        split=config.dataset.dataset_split,
-        cache_dir=config.system.hf_cache,
-    )
+    # MATH dataset
+    if config.dataset.dataset_path == "EleutherAI/hendrycks_math":
+        configs = get_dataset_config_names(config.dataset.dataset_path)
+        datasets_by_subset = {
+            cfg: load_dataset(config.dataset.dataset_path, cfg, split="test")
+            for cfg in configs
+        }
+        dataset = concatenate_datasets(list(datasets_by_subset.values()))
+        dataset = dataset.rename_columns({
+            "problem": "question",
+            "solution": "answer"
+        })
+    # proofNet dataset
+    elif config.dataset.dataset_path == "hoskinson-center/proofnet":
+        dataset = load_dataset(
+            config.dataset.dataset_path,
+            split=config.dataset.dataset_split,
+            cache_dir=config.system.hf_cache,
+        )
+        dataset = dataset.rename_columns({
+            "nl_statement": "question",
+            "nl_proof": "answer"
+        })
+    else:
+        dataset = load_dataset(
+            config.dataset.dataset_path,
+            config.dataset.dataset_config,
+            split=config.dataset.dataset_split,
+            cache_dir=config.system.hf_cache,
+        )
+       
     if config.dataset.subset:
         dataset = dataset.select(range(min(config.dataset.subset, len(dataset))))
 
