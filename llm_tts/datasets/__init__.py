c739b813283e64cd8436d9317875e19dde27dba7
ssenichev <tsowehh@gmail.com>
2026-02-02 18:26:14 +0300

    init updates

--- llm_tts/datasets/__init__.py
+++ llm_tts/datasets/__init__.py
@@ -6,10 +6,26 @@ from .gsm8k import (
     format_gsm8k_for_deepconf,
     load_gsm8k,
 )
+from .swe_bench import (
+    SWEBenchInstance,
+    create_prediction_file,
+    extract_patch_from_response,
+    format_swe_bench_prompt,
+    load_predictions_file,
+    load_swe_bench_lite,
+)
 
 __all__ = [
+    # GSM8K
     "load_gsm8k",
     "evaluate_gsm8k_answer",
     "extract_answer_from_gsm8k",
     "format_gsm8k_for_deepconf",
+    # SWE-bench
+    "load_swe_bench_lite",
+    "SWEBenchInstance",
+    "format_swe_bench_prompt",
+    "extract_patch_from_response",
+    "create_prediction_file",
+    "load_predictions_file",
 ]
--- llm_tts/evaluation/__init__.py
+++ llm_tts/evaluation/__init__.py
@@ -1,5 +1,12 @@
 from .alignscore import EvaluatorAlignScore
 from .exact_match import EvaluatorExactMatch
 from .llm_as_a_judge import EvaluatorLLMAsAJudge
+from .swe_bench_evaluator import EvaluatorSWEBench, run_swebench_evaluation_cli
 
-__all__ = ["EvaluatorLLMAsAJudge", "EvaluatorExactMatch", "EvaluatorAlignScore"]
+__all__ = [
+    "EvaluatorLLMAsAJudge",
+    "EvaluatorExactMatch",
+    "EvaluatorAlignScore",
+    "EvaluatorSWEBench",
+    "run_swebench_evaluation_cli",
+]
