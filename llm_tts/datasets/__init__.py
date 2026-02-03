8f7e5c65db59185b234a944a1a825f3fe7ce1f42
ssenichev <tsowehh@gmail.com>
2026-02-03 16:49:29 +0300

    inits

--- llm_tts/datasets/__init__.py
+++ llm_tts/datasets/__init__.py
@@ -6,10 +6,24 @@ from .gsm8k import (
     format_gsm8k_for_deepconf,
     load_gsm8k,
 )
+from .mbpp_plus import (
+    create_evalplus_samples,
+    extract_code_from_response,
+    format_mbpp_prompt,
+    load_evalplus_samples,
+    load_mbpp_plus,
+)
 
 __all__ = [
+    # GSM8K
     "load_gsm8k",
     "evaluate_gsm8k_answer",
     "extract_answer_from_gsm8k",
     "format_gsm8k_for_deepconf",
+    # MBPP+
+    "load_mbpp_plus",
+    "extract_code_from_response",
+    "format_mbpp_prompt",
+    "create_evalplus_samples",
+    "load_evalplus_samples",
 ]
--- llm_tts/evaluation/__init__.py
+++ llm_tts/evaluation/__init__.py
@@ -1,5 +1,11 @@
 from .alignscore import EvaluatorAlignScore
 from .exact_match import EvaluatorExactMatch
 from .llm_as_a_judge import EvaluatorLLMAsAJudge
+from .mbpp_plus_evaluator import EvaluatorMBPPPlus
 
-__all__ = ["EvaluatorLLMAsAJudge", "EvaluatorExactMatch", "EvaluatorAlignScore"]
+__all__ = [
+    "EvaluatorLLMAsAJudge",
+    "EvaluatorExactMatch",
+    "EvaluatorAlignScore",
+    "EvaluatorMBPPPlus",
+]
