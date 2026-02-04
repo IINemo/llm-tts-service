90d6524cc5bfd98747ecf668ae07510451a394c9
VICTOR MAZANOV <v.mazanov@innopolis.university>
2026-02-04 21:10:20 +0300

    fix: added back import of step scorer prm

--- llm_tts/scorers/__init__.py
+++ llm_tts/scorers/__init__.py
@@ -1,5 +1,6 @@
 from .majority_voting import ChainMajorityVotingScorer, MajorityVotingScorer
 from .step_scorer_confidence import StepScorerConfidence
+from .step_scorer_prm import StepScorerPRM
 from .step_scorer_self_verification import StepScorerSelfVerification
 from .step_scorer_uncertainty import StepScorerUncertainty
 from .tree_of_thoughts import TotStateScorerBase, TotValueScorer, TotVoteScorer
