455a278c7a31d30c0b716ab23c59f406ca68d187
s.senichev <tsowehh@gmail.com>
2026-02-21 14:19:03 +0300

    upd

--- scripts/slurm/mur_he_plus.sh
+++ scripts/slurm/mur_he_plus.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=[MARS-3554]mur_he_plus_seq
+#SBATCH --job-name=[MARS-3554]mur_he_plus_entropy
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=20
-#SBATCH --mem=64gb
+#SBATCH --mem=40gb
 #SBATCH --gres=gpu:1
 #SBATCH --partition=mts_ai
 
 srun --container-image=artifactory.mts.ai/ml-docker/gpt_transformers_pytorch_24_12 \
-     --container-name=mur_he_plus_seq \
+     --container-name=mur_he_plus_entropy \
      --no-container-entrypoint \
      --container-workdir=/home/s.senichev/llm-tts-service/ \
      bash -c "
@@ -18,9 +18,9 @@ srun --container-image=artifactory.mts.ai/ml-docker/gpt_transformers_pytorch_24_
        pip install latex2sympy2 --no-deps &&
        pip install -e '.[dev,vllm]' &&
 
-       echo '=== Running MUR (adaptive scaling): seq ===' &&
+       echo '=== Running MUR (adaptive scaling) ===' &&
        python scripts/run_tts_eval.py \
          --config-path=../config \
-         --config-name=experiments/adaptive_scaling/human_eval_plus/adaptive_scaling_vllm_qwen3_8b_human_eval_plus_sequence_prob \
+         --config-name=experiments/adaptive_scaling/human_eval_plus/adaptive_scaling_vllm_qwen3_8b_human_eval_plus_entropy \
          report_to=none generation.checkpoint_batch_size=32
      "
